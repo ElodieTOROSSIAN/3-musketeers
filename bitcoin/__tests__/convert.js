9fee6bb31e269e952021c6e41ed5296a4610030a
Unknown <diidiie159@gmail.com>
2018-03-07 19:13:29 +0100

    test

--- bitcoin/__tests__/convert.js
+++ bitcoin/__tests__/convert.js
@@ -4,33 +4,28 @@ const convert = require('..');
 const Big = require('big.js');
 
 test('should default to returning a Number', () => {
-  //convert(2, 'BTC', 'BTC');
-  throw new Error('test not yet defined... write your test here');
+	expect(typeof convert(2, 'BTC', 'BTC')).toBe('number');
 });
 
 test('should return a Number', () => {
-  //convert(2, 'BTC', 'BTC', 'Number');
-  throw new Error('test not yet defined... write your test here');
+	expect(typeof convert(2, 'BTC', 'BTC', 'Number')).toBe('number');
+	//throw new Error('test not yet defined... write your test here');
 });
 
 test('should return a Big number', () => {
-  //convert(2, 'BTC', 'BTC', 'Big');
-  throw new Error('test not yet defined... write your test here');
+	expect(typeof convert(2, 'BTC', 'BTC', 'Big')).toBe('big');
 });
 
 test('should return a String', () => {
-  //convert(2100, 'mBTC', 'BTC', 'String');
-  throw new Error('test not yet defined... write your test here');
+	expect(typeof convert(2100, 'mBTC', 'BTC', 'String')).toBe('string');
 });
 
 test('should convert an integer', () => {
-  //convert(123456789012345, 'Satoshi', 'BTC', 'Number');
-  throw new Error('test not yet defined... write your test here');
+	expect(typeof convert(123456789012345, 'Satoshi', 'BTC', 'Number')).toBe('number');
 });
 
 test('should convert a number', () => {
-  //convert(1234567.89012345, 'BTC', 'Satoshi', 'Number');
-  throw new Error('test not yet defined... write your test here');
+  expect(typeof convert(1234567.89012345, 'BTC', 'Satoshi', 'Number')).toBe('number');
 });
 
 test('should convert a string', () => {
